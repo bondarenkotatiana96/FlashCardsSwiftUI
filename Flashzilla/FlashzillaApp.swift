f6131c2c982c360ea6fb20f99968b0fcb3a64977
Tatiana Bondarenko <62190724+bondarenkotatiana96@users.noreply.github.com>
2023-06-03 16:11:13 -0700

    Add files via upload

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by user on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
